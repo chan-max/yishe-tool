16d35e2c07b961e55dd5b74d7f632725f3fca99e
chan-max <2651308363@qq.com>
2023-12-01 21:31:23 +0800

    dev

--- server/sequelize/config/config.js
+++ server/sequelize/config/config.js
@@ -1,16 +1,14 @@
 const dotenv = require('dotenv')
-dotenv.config({ path: `${process.cwd()}/.env`})
-console.log(`${process.cwd()}/.env`)
+
+dotenv.config()
 
 module.exports = {
-  get development(){
-    return {
-      username: process.env.mysql_username,
-      password: process.env.mysql_password,
-      database: process.env.mysql_database,
-      host: "127.0.0.1",
-      dialect: "mysql",
-    }
+  development: {
+    username: process.env.mysql_username ,
+    password: process.env.mysql_password ,
+    database: process.env.mysql_database ,
+    host: "127.0.0.1",
+    dialect: "mysql",
   },
   test: {
     username: process.env.mysql_username,

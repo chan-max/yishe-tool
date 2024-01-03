b62f0ccc6b119709f89f0223875b5e2c10cdba53
chan-max <jackieontheway666@gmail.com>
2024-01-03 20:47:19 +0800

    dev

--- src/modules/app/views/login/index.vue
+++ src/modules/app/views/login/index.vue
@@ -2,23 +2,36 @@
  * @Author: chan-max jackieontheway666@gmail.com
  * @Date: 2024-01-01 21:36:27
  * @LastEditors: chan-max jackieontheway666@gmail.com
- * @LastEditTime: 2024-01-02 22:59:41
+ * @LastEditTime: 2024-01-03 20:43:40
  * @FilePath: /1s/src/modules/app/views/login/index.vue
  * @Description: 
  * 
  * Copyright (c) 2024 by 1s, All Rights Reserved. 
 -->
 <template>
-  <ion-header>
-    <ion-toolbar>
-      <ion-title>登录</ion-title>
-    </ion-toolbar>
-  </ion-header>
-  <ion-content :fullscreen="true" class="ion-padding">
-    <el-input placeholder="账号" v-model="form.account" size="large"></el-input>
-    <el-input placeholder="密码" v-model="form.password" size="large"></el-input>
-    <el-button @click="submit"> 登录 </el-button>
-  </ion-content>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-back-button default-href="/" text="首页"> </ion-back-button>
+        </ion-buttons>
+        <ion-title>登录</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true" class="ion-padding">
+      <h1>登录到衣设</h1>
+      <ion-input
+        label="账号"
+        label-placement="floating"
+        placeholder="账号或邮箱手机号"
+      ></ion-input>
+      <ion-input
+        label="密码"
+        label-placement="floating"
+        placeholder="密码在6到18位"
+      ></ion-input>
+    </ion-content>
+  </ion-page>
 </template>
 <script setup lang="ts">
 import { ref, reactive } from "vue";
@@ -31,7 +44,9 @@ import {
   IonButton,
   IonButtons,
   IonBackButton,
+  IonInput
 } from "@ionic/vue";
+import { chevronBackOutline } from "ionicons/icons";
 import { login } from "@/api";
 
 const form = reactive({ account: "", password: "" });

801237dee320f8f9517cd26057da54e4f62b458e
chan-max <jackieontheway666@gmail.com>
2024-01-28 10:23:19 +0800

    allowheaders

--- server/index.js
+++ server/index.js
@@ -47,7 +47,7 @@ app.use(
         origin: "*",
         credentials: true, //是否允许发送Cookie
         allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], //设置所允许的HTTP请求方法
-        allowHeaders: () => '*', //设置服务器支持的所有头信息字段
+        allowHeaders: '*', //设置服务器支持的所有头信息字段
         exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'] //设置获取其他自定义字段
     })
 );
